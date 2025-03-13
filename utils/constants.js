8ab5f355443b378bab9dee4c739527b1a17de4ae
jethalal62 <jgada221@outlook.com>
2025-03-13 16:33:22 +0530

    Create constants.js

---
+++ utils/constants.js
@@ -0,0 +1,11 @@
+export const CONTENT_TYPES = {
+  MOVIE: 'movie',
+  TVSHOW: 'tvshow',
+  ANIME: 'anime',
+  ADULT: 'adult',
+}
+
+export const VIDEO_SOURCES = {
+  STREAMHG: 'streamhg',
+  RPMSHARE: 'rpmshare',
+}
